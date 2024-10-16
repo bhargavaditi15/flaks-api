4e39645f9da5e093ca0f45b2b7a7ff02132d8fda
ADITI BHARGAVA <104289889+bhargavaditi15@users.noreply.github.com>
2024-10-17 00:43:52 +0530

    test.py

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import requests
 
-response = requests.get('http://127.0.0.1:5000/app/api/details/all')
+
+response = requests.get('http://192.168.31.26:5000/app/api/details/all')
 print(response.status_code)
 print(response.json())
+
+
+# response = requests.get('http://127.0.0.1:5000/app/api/details/all')
+# print(response.status_code)
+# print(response.json())
 
 # response = requests.get('http://127.0.0.1:5000/app/api/courses?id=1')
 # print(response.status_code)
